4b5a89e2bc51b42cbcd97902ec857e8844ae4bf0
XimenZangief <s9671073@gmail.com>
2021-10-25 16:15:21 +0800

    calendar v0.9

---
+++ php/calendar.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>線上月曆</title>
+    <style>
+        table{
+            width: 500px;
+margin: auto;
+padding: 20px;
+border: 1px solid black;
+        }
+        td{
+            padding: 5px;
+            border: 1px solid black;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>線上月曆</h1>
+    <table>
+        <tr>
+            <td>日</td>
+            <td>一</td>
+            <td>二</td>
+            <td>三</td>
+            <td>四</td>
+            <td>五</td>
+            <td>六</td>
+        </tr>
+    <?php
+    for($i=0;$i<6;$i++)
+    {
+        echo '<tr>';
+        for($j=0;$j<7;$j++){
+            if($i==0 && $j<5)
+                echo '<td></td>';
+            else if($i==5 && $j>0)
+                echo '<td></td>';
+            else
+                echo '<td>'. ($i*7+$j+1-5) .'</td>';
+        }
+    }
+    ?>
+</body>
+</html>
