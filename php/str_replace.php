f79146926e4a423518b9d8f5fee24d6c3c9bb25b
XimenZangief <s9671073@gmail.com>
2021-10-25 10:25:04 +0800

    str_explode

---
+++ php/str_replace.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>string_replace</title>
+</head>
+<body>
+    <?php
+        $str='aaa132323121y';
+        echo "str_repeat(str_replace($str, '*', $str),strlen($str))".'<br>';
+        echo str_repeat(str_replace($str, '*', $str),strlen($str));
+        echo '<hr>';
+        echo "str_repeat('*', strlen($str))".'<br>';
+        echo str_repeat('*', strlen($str)); //ezMode
+        echo '<hr>';
+
+        echo "str_replace('a','*',$str)".'<br>';
+        echo str_replace('a','*',$str);
+        echo '<hr>';
+
+        $array=[];
+        for($i=0; $i<strlen($str); $i++){
+            $s=substr($str, $i,1);
+            if(!in_array($s,$array)){
+                $array[]=$s;
+            }
+        }
+        echo "str_replace($array, '*', $str)" . '<br>';
+        echo str_replace($array, '*', $str);
+    ?>
+</body>
+</html>
