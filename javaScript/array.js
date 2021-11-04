d59e285d998f1b3f2b403c7c651d238a7c6e27d5
XimenZangief <s9671073@gmail.com>
2021-11-04 10:01:10 +0800

    new file:   array.js (new js) 	modified:   js_211103_starbucks.html (merge with js) 	modified:   prac1.html (debug)

---
+++ javaScript/array.js
@@ -0,0 +1,21 @@
+console.log(typeof({}));
+
+const myObject = {
+    name: "amy",
+    age: 18,
+    tel: 0912345678,
+    isAdmin: false
+}
+
+const myObject2 = {
+    name: "bob",
+    age: 18,
+    tel: 0912333444,
+    isAdmin: false
+}
+console.log("obj", myObject);
+console.log(myObject.isAdmin);
+console.log(myObject['name']);
+console.log("obj", myObject2);
+console.log(myObject2.isAdmin);
+console.log(myObject2['name']);
